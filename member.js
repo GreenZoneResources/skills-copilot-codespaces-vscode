f5bd10f86e452f1b18939b321907c90dcb981775
GreenZoneResources <akereledayo94@gmail.com>
2023-08-04 03:33:29 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,1 +1,16 @@
-function skillsMember()
+function skillsMember() {
+  const skills = document.getElementById('skills');
+  if (skills) {
+    skills.addEventListener('click', (event) => {
+      if (event.target.classList.contains('btn-remove')) {
+        const skillId = event.target.getAttribute('data-skill-id');
+        const memberId = event.target.getAttribute('data-member-id');
+        axios
+          .post(`/members/${memberId}/skills/${skillId}/remove`)
+          .then((response) => {
+            event.target.parentElement.remove();
+          });
+      }
+    });
+  }
+}
